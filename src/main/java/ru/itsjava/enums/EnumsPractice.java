e56862c44c53c62f9626dfaecdc601953b9d4432
Vladimir <300wrongaway@mail.ru>
2020-12-09 16:22:45 +0300

    Create Enum Weekdays

--- src/main/java/ru/itsjava/enums/EnumsPractice.java
+++ src/main/java/ru/itsjava/enums/EnumsPractice.java
@@ -2,13 +2,17 @@ package ru.itsjava.enums;
 
 public class EnumsPractice {
     public static void main(String[] args) {
-        System.out.println(Season.WINTER);
-        Season[] seasons = Season.values();
-        for (Season season: seasons){
-            System.out.println(season);
-        }
-        System.out.println(Season.valueOf("WINTER"));
+//        System.out.println(Season.WINTER);
+//        Season[] seasons = Season.values();
+//        for (Season season: seasons){
+//            System.out.println(season);
+//        }
+//        System.out.println(Season.valueOf("WINTER"));
+//
+//        System.out.println(WinterMonth.DECEMBER);
 
-        System.out.println(WinterMonth.DECEMBER);
+
+        System.out.println("Weekdays.MONDAY = " + Weekdays.MONDAY);
+        System.out.println("Weekdays.FRIDAY = " + Weekdays.FRIDAY);
     }
 }
